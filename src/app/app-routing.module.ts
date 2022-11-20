dd167c003ea8ef84876458dc62dca8f4b1dba946
shoeb-gg <1920038@iub.edu.bd>
2022-11-20 13:41:23 +0600

    trying to fix route error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { StopwatchComponent } from './components/stopwatch/stopwatch.component';
 import { TimerComponent } from './components/timer/timer.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/timer', pathMatch: 'full' },
+  { path: '', redirectTo: 'timer', pathMatch: 'full' },
   { path: 'timer', component: TimerComponent },
   { path: 'stopwatch', component: StopwatchComponent },
 ];
